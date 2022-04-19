648626b7f9872756eaa844cb31a97268efa592e8
Ruslan <kornerus@fjfi.cvut.cz>
2022-04-20 00:16:38 +0300

    add get all divisions request

--- src/spbu_api/study_divisions.py
+++ src/spbu_api/study_divisions.py
@@ -1,9 +1,24 @@
+from typing import Generator, NamedTuple
 from spbu_api import SpbuApi
+
+
+class Division(NamedTuple):
+    oid: int
+    alias: str
+    name: str
 
 
 class StudyDivisionsApi(SpbuApi):
     def __init__(self):
         self.API = f"{self.API}study/divisions/"
 
-    def get_all(self):
-        return self.send_query(self.API)
+    def get_all(self) -> Generator[Division, None, str]:
+        response = self.send_query(self.API)
+        if 200 < response.code and response.code < 400:
+            for division in response.response:
+                yield Division(
+                    oid=division.get("Oid", None),
+                    alias=division.get("Alias", None),
+                    name=division.get("Name", None)
+                )
+        return response.status
