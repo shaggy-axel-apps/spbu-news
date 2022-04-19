9ea6ab26ede2bd93ed55ab7885af1e90c5814ce1
Ruslan <kornerus@fjfi.cvut.cz>
2022-04-20 02:59:12 +0300

    refactoring and debug async changes

--- src/spbu_api/base.py
+++ src/spbu_api/base.py
@@ -1,20 +1,14 @@
-from typing import NamedTuple, Optional, Union
+from typing import NamedTuple
 
 import aiohttp
-from requests.models import Response
-from requests.status_codes import codes
-from requests.structures import LookupDict
 
 from settings.const import SPBU_API
-
-
-codes: LookupDict
 
 
 class ApiResponse(NamedTuple):
     code: int
     status: str
-    response: Optional[Union[Response, list[dict], dict, str]]
+    response: list[dict]
 
 
 class SpbuApi:
@@ -25,7 +19,7 @@ class SpbuApi:
         async with aiohttp.ClientSession() as session:
             response = await session.get(api.format(**params))
             return ApiResponse(
-                code=response.status_code,
-                status=codes.get(response.status_code, ("unknown",))[0],
+                code=response.status,
+                status=response.reason,
                 response=response.json()
             )
--- src/spbu_api/study_divisions.py
+++ src/spbu_api/study_divisions.py
@@ -1,4 +1,4 @@
-from typing import Generator, NamedTuple
+from typing import NamedTuple, Union
 from spbu_api import SpbuApi
 from spbu_api import ApiResponse
 
@@ -22,20 +22,23 @@ class Program(NamedTuple):
 
 class StudyDivisionsApi(SpbuApi):
     def __init__(self):
+        super().__init__()
         self.API = f"{self.API}study/divisions/"
 
-    async def get_all(self) -> Generator[Division, None, str]:
+    async def get_all(self) -> Union[list[Division], str]:
         response = await self.send_query(self.API)
         if 200 <= response.code and response.code < 400:
-            for division in response.response:
-                yield Division(
+            divisions = []
+            for division in await response.response:
+                divisions.append(Division(
                     oid=division.get("Oid", None),
                     alias=division.get("Alias", None),
                     name=division.get("Name", None)
-                )
+                ))
+            return divisions
         return response.status
 
-    async def get_programs(self, alias: str) -> Generator:
+    async def get_programs(self, alias: str) -> list[Program]:
         """ Gets study programs with levels for a given study division """
         response: ApiResponse = await self.send_query(
             "{api}{alias}programs/levels/",
@@ -63,3 +66,4 @@ class StudyDivisionsApi(SpbuApi):
                         for year in program["AdmissionYears"]
                     ]
             return programs
+        response.status
