074edccadf82ba58dc6b0b0a9b1bf9934e9a21b7
Ruslan <kornerus@fjfi.cvut.cz>
2022-04-21 23:07:26 +0300

    add cache settings

--- src/settings/const.py
+++ src/settings/const.py
@@ -1,16 +1,20 @@
 import os
 
 
-# Environ Variables
+# TELEGRAM BOT
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMINS = tuple(map(int, os.environ.get("ADMINS").split(',')))
 USE_REDIS = bool(os.environ.get("USE_REDIS"))
 
+# DATABASE
 POSTGRES_DB = os.environ.get('POSTGRES_DB')
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
 POSTGRES_PASS = os.environ.get('POSTGRES_PASS')
 
-PROGRAMS_ID_FROM = 10_000
-PROGRAMS_ID_TO = 10_263
+# REDIS
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = int(os.environ.get('REDIS_PORT'))
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 
+# API
 SPBU_API = "https://timetable.spbu.ru/api/v1/"
